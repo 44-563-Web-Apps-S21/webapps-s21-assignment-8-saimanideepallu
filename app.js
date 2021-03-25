74c248495a46d611c95df52f62c72cc3b1c335e8
saimanideepallu <manideepmlalgo@gmail.com>
2021-03-25 10:04:19 -0500

    updated

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-const http = require('http')    //Pull in a useful node package
-                                //Try http.
-const hostname = '127.0.0.1'    //Local host
-const port = 3001               //Not assigned
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const server =
-  http.createServer(            //Creates the response loop
-    (req,res)=> {               //Anonymous function to handle the request
-      res.statusCode = 200      //code for OK
-      res.setHeader('Content-Type', 'text/plain') //Set the mime type
-      res.end('Feel every moment')    //Close the response and provide content
-    }                           //No return needed, we modified the res object we got
-)
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-server.listen(port, hostname, () => {   //Start the server
-  console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
-})
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
